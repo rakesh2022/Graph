8557b6bf605f2089de12d8914120840e8022f925
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-09-27 01:43:00 +0530

    first commit

---
+++ 5_Matrix.cpp
@@ -0,0 +1,35 @@
+
+https://leetcode.com/problems/01-matrix/
+
+
+// Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+// The distance between two adjacent cells is 1.
+// example
+// Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+// Output: [[0,0,0],[0,1,0],[1,2,1]]
+ 
+
+
+class Solution {
+public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        queue<pair<int, int>> q;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (mat[r][c] == 0) q.emplace(r, c);
+                else mat[r][c] = -1; // Marked as not processed yet!
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + DIR[i], nc = c + DIR[i+1];
+                if (min(nr,nc)<0 || nr == m || nc == n || mat[nr][nc] != -1) continue;
+                mat[nr][nc] = mat[r][c] + 1;
+                q.emplace(nr, nc);
+            }
+        }
+        return mat;
+    }
+};
