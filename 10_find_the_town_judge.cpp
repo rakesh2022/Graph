2a772a5b7307982022df1c385885fb572b5253f2
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-09-28 00:28:35 +0530

    first commit

---
+++ 10_find_the_town_judge.cpp
@@ -0,0 +1,13 @@
+
+https://leetcode.com/problems/find-the-town-judge/
+
+int findJudge(int N, vector<vector<int>> &trust)
+{
+  vector<int> balance(N + 1);
+  for (auto &t : trust)
+    --balance[t[0]], ++balance[t[1]];
+  for (auto i = 1; i <= N; ++i)
+    if (balance[i] == N - 1)
+      return i;
+  return -1;
+}
