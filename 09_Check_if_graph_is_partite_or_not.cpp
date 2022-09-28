89433ed1b6a03dc8e349f976e07c38b7f464933d
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-09-28 19:03:52 +0530

    second commit

--- 09_Check_if_graph_is_partite_or_not.cpp
+++ 09_Check_if_graph_is_partite_or_not.cpp
@@ -64,16 +64,15 @@ bool isBipartite(int V, vector<int>adj[]) {
     TC: O(V + E)
     SC: O(V + E)
 */
-bool dfs(int curr, int parent, vector<int>& color, vector<int> adj[]) {
-    color[curr] = 1 - color[parent];
+bool dfs(int curr, int col, vector<int>& color, vector<int> adj[]) {
+    color[curr]=col;
 
     for (int i : adj[curr]) {
         if (color[i] == -1) {
-            color[i] = 1 - color[curr];
-            if (!dfs(i, curr, color, adj)) return false;
+            if (!dfs(i, 1-color[curr], color, adj)) return false;
         }
         // color is same as well as it is not parent
-        else if (color[i] == color[curr] && i != parent) return false;
+        else if (color[i] == color[curr]) return false;
     }
 
     return true;
@@ -84,7 +83,7 @@ bool isBipartite(int V, vector<int>adj[]) {
 
     for (int i = 0;i < V;i++) {
         if (color[i] == -1) {
-            if (!dfs(i, -1, color, adj)) return false;
+            if (!dfs(i,0, color, adj)) return false;
         }
     }
     return true;
